49bf478c40833ea9b0895591a6d73df3fdc2de9f
Bryan <bryan.how@live.com>
2022-07-21 20:10:00 +0800

    css adjustments

--- src/components/Comments/NewCommentForm.js
+++ src/components/Comments/NewCommentForm.js
@@ -3,18 +3,21 @@ import UseAvatar from "../../UseAvatar";
 
 // mui setup
 import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 
-const NewCommentForm = ({postId, user, handleCommentSend}) => {
+var async = require('async');
+
+const NewCommentForm = ({postId, user, handleCommentSend, getPosts}) => {
     const [commentContent, setCommentContent] = useState('');
 
-    const handleKeyPress = (event) => {
+    const handleKeyPress = async (event) => {
         if (event.key === 'Enter') {
-            handleCommentSend(postId, commentContent);
+            await handleCommentSend(postId, commentContent);
             setCommentContent('');
+            // getPosts('');
         };
     };
 
-    // might change this to show 'invalid user'
     if (!user) {
         return (
             <div>
@@ -24,16 +27,25 @@ const NewCommentForm = ({postId, user, handleCommentSend}) => {
     };
 
     return (
-        <div>
-            <UseAvatar user={user} />
+        <Box sx={{
+            margin: '8px', 
+            width: '90%', 
+            maxWidth: '100%', 
+            display: 'flex', 
+            flexDirection: 'row', 
+            justifyContent: 'flex-start',
+            }}>
+            <UseAvatar user={user}/>
             <TextField 
-                variant='standard'
+                sx={{marginLeft: '8px'}}
+                fullWidth
+                variant='outlined'
                 placeholder="Enter you comment here"
                 onKeyPress={handleKeyPress}
                 onChange={(e) => setCommentContent(e.target.value)}
                 value={commentContent}
             />
-        </div>
+        </Box>
     );
 };
 
