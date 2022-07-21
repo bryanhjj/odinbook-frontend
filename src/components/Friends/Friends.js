76024e23847eaeebec94846acf37cb3525e1bc62
Bryan <bryan.how@live.com>
2022-07-21 20:42:38 +0800

    bug fixes

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -14,73 +14,69 @@ const Friends = ({user}) => {
     const [userFriends, setUserFriends] = useState([]);
     const [friendRequests, setFriendRequests] = useState([]);
     const {handleAcceptRequest, handleDenyRequest} = axiosFns({
-        userFriends,
-        setUserFriends,
-        friendRequests,
-        setFriendRequests,
+        userFriends: userFriends,
+        setUserFriends: setUserFriends,
+        friendRequests: friendRequests,
+        setFriendRequests: setFriendRequests,
     });
 
     useEffect(() => {
-        axios
-        .get(`/users/${user.id}`)
-        .then((results) => {
-            // to modify this to include friend_req_sent
-            setUserFriends(results.data.user.friend_list);
-            setFriendRequests(results.data.user.friend_req_rec);
-        });
+        if (user) {
+            axios
+            .get(`/users/${user.id}`)
+            .then((results) => {
+            setUserFriends(results.data.user.user.friend_list);
+            setFriendRequests(results.data.user.user.friend_req_rec);
+            });
+        }
     }, [user]);
 
     return(
-        <div>
-            <Container maxWidth="x1">
-                <Grid container spacing={3}>
-                    <Grid item xs={12} md={9}>
-                        <Typography variant="h6">
-                            Search for other users
-                        </Typography>
-                        <UserSearch />
-                        {friendRequests ? (
-                            <Typography variant="h5">
-                                Friend requests received
-                            </Typography>
-                        ) : null }
-                        {friendRequests.length > 0 ? (
-                            friendRequests.map((fren) => {
-                                return (
-                                    <FriendRequestCard 
-                                        friend={fren}
-                                        key={fren._id}
-                                        handleAcceptRequest={handleAcceptRequest}
-                                        handleDenyRequest={handleDenyRequest}
-                                    />
-                                );
-                            })
-                        ) : (
-                            <Typography variant="body1">
-                                You do you have any friend requests at this moment.
-                            </Typography>
-                        )}
+        <Container maxWidth="x1" sx={{minHeight: '100vh', marginTop: '10px'}}>
+            <Grid container spacing={3} sx={{justifyContent: 'center'}}>
+                <Grid item xs={12} md={9}>
+                    <UserSearch />
+                    {friendRequests ? (
                         <Typography variant="h5">
-                            Friends
+                            Friend requests received
                         </Typography>
-                        {userFriends.length > 0 ? (
-                            userFriends.map((fren) => {
-                                return (
-                                    <FriendCard 
-                                        friend={fren}
-                                        key={fren._id}
-                                    />
-                                );
-                            })
-                        ) : (
-                            <Typography variant="body1">
-                                No friends yet!
-                            </Typography>
-                        )}
-                    </Grid>
+                    ) : null }
+                    {friendRequests.length > 0 ? (
+                        friendRequests.map((fren) => {
+                            return (
+                                <FriendRequestCard 
+                                    friend={fren}
+                                    key={fren._id}
+                                    handleAcceptRequest={handleAcceptRequest}
+                                    handleDenyRequest={handleDenyRequest}
+                                />
+                            );
+                        })
+                    ) : (
+                        <Typography variant="body1">
+                            You do you have any friend requests at this moment.
+                        </Typography>
+                    )}
+                    <Typography variant="h5">
+                        Friends
+                    </Typography>
+                    {userFriends.length > 0 ? (
+                        userFriends.map((fren) => {
+                            return (
+                                <FriendCard 
+                                    friend={fren}
+                                    key={fren._id}
+                                />
+                            );
+                        })
+                    ) : (
+                        <Typography variant="body1">
+                            No friends yet!
+                        </Typography>
+                    )}
                 </Grid>
-            </Container>
-        </div>
+            </Grid>
+        </Container>
     );
 };
 
