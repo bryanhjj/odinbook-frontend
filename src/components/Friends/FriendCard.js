bd21aef0d8f35fdae94383ba035218299e96cf39
Bryan <bryan.how@live.com>
2022-07-21 20:30:47 +0800

    css and bug fixes

--- src/components/Friends/FriendCard.js
+++ src/components/Friends/FriendCard.js
@@ -1,22 +1,31 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import UseAvatar from '../../UseAvatar';
 
 // mui setup
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
 
 const FriendCard = ({friend}) => {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
+    /*
     const handleClick = () => {
         navigate(`/users/${friend._id}`);
     }
+    */
 
     return (
-        <div>
-            <Card>
+        <Paper sx={{
+            backgroundColor: 'rgb(32,33,34)', 
+            color: 'white', 
+            margin: '20px 10px',
+            width: '25%',
+            textAlign: 'center',
+            }}>
+            <Card sx={{display: 'flex', alignItems: 'center',  paddingLeft: '8px'}}>
                 <UseAvatar user={friend} />
                 <CardContent>
                     <Typography variant="body1">
@@ -24,7 +33,7 @@ const FriendCard = ({friend}) => {
                     </Typography>
                 </CardContent>
             </Card>
-        </div>
+        </Paper>
     )
 }
 
