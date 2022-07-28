cf5539c54f49f7c7bf3af57a2c08d76906d44fc7
Bryan <bryan.how@live.com>
2022-07-28 21:34:50 +0800

    css adjustments

--- src/components/Profile/EditProfilePicForm.js
+++ src/components/Profile/EditProfilePicForm.js
@@ -4,8 +4,9 @@ import React, { useState } from "react";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
-const EditProfilePicForm = ({user, toggleEditProfilePic, handleProfilePicUpdate}) => {
+const EditProfilePicForm = ({user, toggleEditImage, handleProfilePicUpdate}) => {
     const [profilePic, setProfilePic] = useState(null);
 
     const handleChange = (e) => {
@@ -13,17 +14,20 @@ const EditProfilePicForm = ({user, toggleEditProfilePic, handleProfilePicUpdate}
     };
 
     return (
-        <div>
-            <Typography variant="h5">Update profile picture</Typography>
+        <Box sx={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
+            <Typography variant="h5" sx={{margin: '10px 8px'}}>
+                Update profile picture
+            </Typography>
             <TextField
+                sx={{margin: 8}}
                 id="new_profile_pic"
                 name="new_profile_pic"
                 type="file"
                 onChange={(e) => handleChange(e)}
             />
-            <Button variant="contained" onClick={handleProfilePicUpdate(profilePic)}>Confirm changes</Button>
-            <Button variant="contained" onClick={toggleEditProfilePic}>Cancel</Button>
-        </div>
+            <Button variant="contained" sx={{margin: '10px 10px'}} onClick={handleProfilePicUpdate(profilePic)}>Confirm changes</Button>
+            <Button variant="contained" sx={{margin: '10px 10px'}} onClick={toggleEditImage}>Cancel</Button>
+        </Box>
     );
 };
 
