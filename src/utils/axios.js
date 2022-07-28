ebdc7e6921ec1e7c34932bf693dff8deb86f63d9
Bryan <bryan.how@live.com>
2022-07-28 21:47:27 +0800

    updated baseURL

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 // Set config defaults when creating the instance, to update accordingly
 const instance = axios.create({
+  //backend server route
   baseURL: "https://odin-book-backend.herokuapp.com/",
+  
+  // for testing purposes
+  // baseURL: "http://localhost:5000"
 });
 
 export default instance;
