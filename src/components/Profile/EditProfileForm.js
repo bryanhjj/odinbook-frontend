ae2a71fb1c1d82f8f4610b815bfbda7f020c3f0f
Bryan <bryan.how@live.com>
2022-07-28 21:33:32 +0800

    switched to Yup for validation

--- src/components/Profile/EditProfileForm.js
+++ src/components/Profile/EditProfileForm.js
@@ -1,15 +1,18 @@
 import React from "react";
 // formik seems interesting, will try it out for this part
-import { useFormik, Formik, Field, Form, ErrorMessage } from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 // mui setup
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 // A custom validation function. This must return an object
 // which keys are symmetrical to our values/initialValues
 // can consider using Yup to help with validation 
+/*
 const validate = values => {
     const errors = {};
     if (!values.first_name) {
@@ -28,20 +31,28 @@ const validate = values => {
 
     return errors;
 };
+*/
+
+const ValidateSchema = Yup.object({
+    first_name: Yup.string().max(20, 'Must be 20 characters or less').required('Required'),
+    last_name: Yup.string().max(20, 'Must be 20 characters or less').required('Required'),
+    email: Yup.string().email('Invalid email address').required('Required'),
+});
 
 const EditProfileForm = ({user, toggleEditProfile, handleProfileUpdate}) => {
     return (
-        <div>
-            <Typography variant="h5">Edit profile</Typography>
+        <Box sx={{display: 'flex', flexWrap: 'wrap'}}>
+            <Typography variant="h5">
+                Edit profile
+            </Typography>
             <Formik
                 initialValues={{
-                    first_name:user.first_name, 
-                    last_name:user.last_name, 
-                    email:user.email, 
-                    phone_number:user.phone_number
+                    first_name: user.first_name, 
+                    last_name: user.last_name, 
+                    email: user.email, 
+                    phone_number: user.phone_number
                 }}
-                // might have to use Yup schema for this since the documentation doesn't show how w/o validationSchema
-                validationSchema={validate}
+                validationSchema={ValidateSchema}
                 onSubmit={(values) => {
                     handleProfileUpdate(values);
                     toggleEditProfile();
@@ -49,55 +60,55 @@ const EditProfileForm = ({user, toggleEditProfile, handleProfileUpdate}) => {
             >
                 {formik => (
                     <Form>
-                        <TextField
+                        <TextField 
+                            sx={{marginTop: '8px'}}
                             variant="outlined"
                             id="first_name"
                             name="first_name"
                             label="First name"
                             type="text"
-                            required={true}
                             {...formik.getFieldProps('first_name')}
                         />
                         <ErrorMessage name="first_name" />
 
                         <TextField
+                            sx={{marginTop: '8px'}}
                             variant="outlined"
                             id="last_name"
                             name="last_name"
                             label="Last name"
                             type="text"
-                            required={true}
                             {...formik.getFieldProps('last_name')}
                         />
                         <ErrorMessage name="last_name" />
 
                         <TextField
+                            sx={{marginTop: '8px'}}
                             variant="outlined"
                             id="email"
                             name="email"
                             label="E-mail"
                             type="email"
-                            required={true}
                             {...formik.getFieldProps('email')}
                         />
                         <ErrorMessage name="email" />
 
                         <TextField
+                            sx={{marginTop: '8px'}}
                             variant="outlined"
                             id="phone_number"
                             name="phone_number"
                             label="Phone number"
                             type="number"
-                            required={true}
                             {...formik.getFieldProps('phone_number')}
                         />
                         <ErrorMessage name="phone_number" />
-                        <Button variant="contained" type="submit" onClick={Formik.handleSubmit}>Confirm changes</Button>
-                        <Button variant="contained" type="reset" onClick={toggleEditProfile}>Cancel</Button> 
+                        <Button variant="contained" type="submit" sx={{margin: '10px 10px'}} onClick={Formik.handleSubmit}>Confirm changes</Button>
+                        <Button variant="contained" type="reset" sx={{margin: '10px 10px'}} onClick={toggleEditProfile}>Cancel</Button> 
                     </Form>
                 )}
             </Formik>
-        </div>
+        </Box>
     );
 };
 
